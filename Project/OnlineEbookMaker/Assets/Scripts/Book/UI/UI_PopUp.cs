39c05cba902044de05c4de7bff62c6196c7a04cf
MrKatsee <ghtjd000129@naver.com>
2021-05-31 18:19:57 +0900

    팝업 UI 추가

---
+++ Project/OnlineEbookMaker/Assets/Scripts/Book/UI/UI_PopUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UI_PopUp : MonoBehaviour {
+    [SerializeField] Text titleText = default;
+    [SerializeField] Text contentText = default;
+
+    [SerializeField] Button yesBtn = default;
+    [SerializeField] Button noBtn = default;
+
+    public void Init(string title, string content, UnityAction onYesClicked, UnityAction onNoClicked) {
+        titleText.text = title;
+        contentText.text = content;
+
+        yesBtn.onClick.AddListener(onYesClicked);
+        noBtn.onClick.AddListener(onNoClicked);
+    }
+}
