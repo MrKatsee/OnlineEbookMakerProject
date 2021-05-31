b10e3beca767feba0034cd73e3d1981426560f67
Ku <rnxotjd123@gmail.com>
2021-05-31 17:28:42 +0900

    Bookshelf controller
    before merging for textmeshprougui

---
+++ Project/OnlineEbookMaker/Assets/Scripts/BookShelf/BookShelfManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controller
+/// </summary>
+public class BookShelfManager : MonoBehaviour
+{
+    public static BookShelfManager Instance { get; private set; }
+    private BookIdContainer _container;
+    private void Awake()
+    {
+        SetSingleTon();
+        LoadBookList();
+    }
+
+    void SetSingleTon()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Debug.LogError("There must be one singleton object at a time!");
+    }
+
+    void LoadBookList()
+    {
+        _container = new BookIdContainer();
+        var idList = BookLoader.LoadBookIdList();
+        foreach (var id in idList)
+        {
+            AddBook(id);
+        }
+    }
+    
+    public void AddBook(string id)
+    {
+        if (StorageChecker.IsStorgaeLimit(_container))
+        {
+            Viewer.Instance.DrawError();
+        }
+        else
+        {
+            Viewer.Instance.DrawBook(id);
+            _container.AddBookId(id);
+            BookLoader.SaveBookIdList(_container.GetBookIdList());
+        }
+    }
+
+    public void OnReadBook(string id)
+    {
+        //todo
+    }
+
+    public void OnRemoveBook(string id)
+    {
+        _container.RemoveBookId(id);
+        BookLoader.SaveBookIdList(_container.GetBookIdList());
+    }
+}
---
+++ Project/OnlineEbookMaker/Assets/Scripts/BookShelf/StorageChecker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StorageChecker
+{
+    const int LIMIT = 12;
+    public static bool IsStorgaeLimit(BookIdContainer idContainer)
+    {
+        return idContainer.Count >= LIMIT;
+    }
+}
